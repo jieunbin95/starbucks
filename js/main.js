3f70c206cfb98d493b22a76533bf54f809cad922
jieunbin95 <bje0416@naver.com>
2023-05-26 13:58:02 +0900

    공동 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,26 +1,3 @@
-const searchEl = document.querySelector('.search')
-// search요소에 후손요소인 input을 가져와야하는 위에 search요소를 찾았기 때문에 document대신 searchEl를 입력해준다
-const searchInputEl = searchEl.querySelector('input')
-
-searchEl.addEventListener('click', function () {
-  // input태그를 마우스로 클릭하여 입력상태로 만든것을 포커스를 얻었다고 한다
-  searchInputEl.focus()
-})
-
-searchInputEl.addEventListener('focus', function () {
-  searchEl.classList.add('focused')
-  // searchInputEl에 속성,속상값을 추가해주는 의미(setAttribute('속성', '속성값')
-  searchInputEl.setAttribute('placeholder', '통합검색')
-})
-
-// 입력상태를 떠난것을 포커스가 벗어났다(blur상태)고 한다
-searchInputEl.addEventListener('blur', function () {
-  searchEl.classList.remove('focused')
-  searchInputEl.setAttribute('placeholder', '')
-})
-
-
-
 const badgeEl = document.querySelector('header .badges')
 const toTopEl=document.querySelector('#to-top')
 
@@ -164,7 +141,3 @@ spyEls.forEach(function (spyEl) {
       .setClassToggle(spyEl,'show')
       .addTo(new ScrollMagic.Controller())
 })
-
-const thisYear=document.querySelector('.this-year')
-// 현재 연도에 맞게 자동 생성해준다
-thisYear.textContent=new Date().getFullYear()
